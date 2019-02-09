c5a5a001241fd1aa0620dfd14328e1c7b54402b2
lhs1553 <lhs1553@gmail.com>
2019-02-09 16:12:07 +0900

    no message

--- push/src/main/java/com/ckpoint/sms/push/token/component/HostHelper.java
+++ push/src/main/java/com/ckpoint/sms/push/token/component/HostHelper.java
@@ -4,11 +4,9 @@ package com.ckpoint.sms.push.token.component;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
-import java.net.*;
-import java.util.Enumeration;
-import java.util.List;
 
 @Component
 @Slf4j
@@ -23,33 +21,13 @@ public class HostHelper {
         return "http://" + this.ip + ":" + this.port;
     }
 
-    private String getHostAddress() throws SocketException, UnknownHostException {
-        InetAddress localAddress = getLocalAddress();
-        if (localAddress == null) {
-            return Inet4Address.getLocalHost().getHostAddress();
-        } else {
-            return localAddress.getHostAddress();
-        }
-    }
-
-    private InetAddress getLocalAddress() throws SocketException {
-        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-        while (networkInterfaces.hasMoreElements()) {
-            List<InterfaceAddress> interfaceAddresses = networkInterfaces.nextElement().getInterfaceAddresses();
-            for (InterfaceAddress interfaceAddress : interfaceAddresses) {
-                InetAddress address = interfaceAddress.getAddress();
-                if (address.isSiteLocalAddress()) {
-                    return address;
-                }
-            }
-        }
-        return null;
-    }
 
     @PostConstruct
-    public void init() throws UnknownHostException, SocketException {
-        this.ip = getHostAddress();
-        log.info(this.ip);
+    public void init() {
+
+        RestTemplate restTemplate = new RestTemplate();
+        this.ip = restTemplate.getForObject("http://bot.whatismyipaddress.com", String.class);
+        log.info(this.getHost());
 
     }
 
