d1c06b639e89a4af8434f4f5152d6d986cec5d79
lhs1553 <lhs1553@gmail.com>
2019-02-09 16:06:50 +0900

    no message

--- push/src/main/java/com/ckpoint/sms/push/token/component/HostHelper.java
+++ push/src/main/java/com/ckpoint/sms/push/token/component/HostHelper.java
@@ -1,14 +1,17 @@
 package com.ckpoint.sms.push.token.component;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.*;
+import java.util.Enumeration;
+import java.util.List;
 
 @Component
+@Slf4j
 public class HostHelper {
 
     @Value("${server.port}")
@@ -20,10 +23,35 @@ public class HostHelper {
         return "http://" + this.ip + ":" + this.port;
     }
 
-    @PostConstruct
-    public void init() throws UnknownHostException {
-        InetAddress local = InetAddress.getLocalHost();
-        this.ip = local.getHostAddress();
+    private String getHostAddress() throws SocketException, UnknownHostException {
+        InetAddress localAddress = getLocalAddress();
+        if (localAddress == null) {
+            return Inet4Address.getLocalHost().getHostAddress();
+        } else {
+            return localAddress.getHostAddress();
+        }
     }
+
+    private InetAddress getLocalAddress() throws SocketException {
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        while (networkInterfaces.hasMoreElements()) {
+            List<InterfaceAddress> interfaceAddresses = networkInterfaces.nextElement().getInterfaceAddresses();
+            for (InterfaceAddress interfaceAddress : interfaceAddresses) {
+                InetAddress address = interfaceAddress.getAddress();
+                if (address.isSiteLocalAddress()) {
+                    return address;
+                }
+            }
+        }
+        return null;
+    }
+
+    @PostConstruct
+    public void init() throws UnknownHostException, SocketException {
+        this.ip = getHostAddress();
+        log.info(this.ip);
+
+    }
+
 
 }
